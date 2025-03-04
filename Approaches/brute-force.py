d61955113be9e009834fccfda1895b387675b0f3
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2025-03-04 13:00:22 -0700

    added a new leetcode problem

--- Approaches/brute-force.py
+++ Approaches/brute-force.py
@@ -11,17 +11,3 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 #Brute Force Approach
 #To check all pairs in nums to see if they sum to the target
-#Time Complexity: O(n^2) because for each element, you are checking all subsequent elements.
-nums = [2,7,11,15]
-target = 9
-
-#excercise
-len(nums)  # Returns 4
-for i in range(len(nums)):  # This will loop through the indices: 0, 1, 2, 3
-    print(i)
-
-#solution
-for i in range(len(nums)):  # Loops through the indices: 0, 1, 2, 3
-    for j in range(i + 1, len(nums)):  # Loops through indices greater than i: 1, 2, 3 (when i = 0), 2, 3 (when i = 1), etc.
-        if nums[i] + nums[j] == target:  # Check if nums[i] + nums[j] equals the target (9)
-            print[i, j]
--- strings.py
+++ strings.py
@@ -14,45 +14,4 @@ print(s[0:10:2])
 #old-style formatting (%)
 name = "Alice"
 age = 25
-print("my name is %s and I am %d years old." %(name, age)) 
-#%s → Stands for string and is used to insert a string value. %d → Stands for integer and is used to insert an integer value.
-
-#format() Method (more verbose)
-
-print("my name is {} and I am {} years old.".format(name, age))
-print("my name is {1} and I am {0} years old.".format(name, age)) #reordering
-print("my name is {name} and I am {age} years old.".format(name='Alice', age=25)) #named placeholders
-
-#f-strings (faster and more readable)
-print(f'my name is {name} and I am{age} years old') 
-print(f"next year, I will be {age + 1} years old.") 
-
-#Regex Functions
-
-import re
-
-#re.match(pattern, string) - checks if the pattern appears AT THE BEGINNING of the string
-result = re.match(r"Hello", "Hello, world!")
-print(result)
-print(bool(result))
-
-#re.search(pattern, string) - finds the FIRST occurence of a pattern ANYWHERE in the string
-result = re.search(r'world', 'hello, world')
-print(result)
-print(bool(result))
-
-#re.findall(pattern, string) - returns ALL occurences of a pattern in a list
-result = re.findall(r"\d+", "There are 43 apples and 5 oranges") #\d+ matches one or more digits in a row
-print(result)
-
-#re.sub(pattern, replacement, string) - replaces occurences of a pattern
-result = re.sub(r"\d+", "XX", "there are 42 apples and 7 oranges.")
-print(result)
-
-text = "contact me at john.doe@example.com or support@company.com"
-emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", text)
-print(emails)
-
-#String Manipulation
-s = "Hello World"
-print(s.lower(), s.upper(), s[::-1])
+print("my name is %s and I am %d years old." %(name, age))
